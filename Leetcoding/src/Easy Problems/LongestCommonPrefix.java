f3a775e202743996b7ab37e8c8b3d24a1e42f6cf
samwanyua <samexample8@gmail.com>
2024-05-29 10:43:36 +0300

    Solve Longest common prefix

---
+++ Leetcoding/src/Easy Problems/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String common = "";
+        if(strs.length == 0) return common;
+        common = strs[0]; // common prefix
+        for(int i =1; i < strs.length; i++){
+            while(strs[i].indexOf(common) != 0){ // checks the position of the substring 
+                common = common.substring(0, common.length() -1);
+            }
+        }
+        return common;
+    }
+}
