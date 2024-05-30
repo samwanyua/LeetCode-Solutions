4e6457cc86e0ff651d7d17e1d67a11b3b0cd7d5d
samwanyua <samexample8@gmail.com>
2024-05-30 15:41:16 +0300

    Solve 3 sum smallest  problem

---
+++ Leetcoding/src/TwoPointers/ThreeSumSmaller.java
@@ -0,0 +1,27 @@
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class ThreeSumSmaller {
+    class Solution{
+        public int threeSumSmaller(int [] nums, int target){
+            Arrays.sort(nums);
+
+            int counter = 0;
+            for(int i = 0; i < nums.length; i++){
+                int left = i + 1, right = nums.length -1;
+                while(left < right){
+                    int currentSum = nums[left] + nums[right] + nums[i];
+                    if(currentSum < target){
+                        counter += right - left;
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+            return counter;
+        }
+    }
+}
