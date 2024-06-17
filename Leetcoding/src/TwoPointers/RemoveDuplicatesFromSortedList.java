0eedb8442828577cbe033a958146e61910c1651c
samwanyua <samexample8@gmail.com>
2024-06-17 17:59:43 +0300

    Solve remove duplicates from sorted list

---
+++ Leetcoding/src/TwoPointers/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,39 @@
+package TwoPointers;
+
+// import Easy Problems.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    
+//   Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode previous = dummy, current = head;
+
+        while(current != null){
+            if(current.next != null && current.val == current.next.val){
+                while(current.next != null && current.val == current.next.val){
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            else{
+                previous = current;
+            }
+            current = current.next;
+        }
+        return dummy.next;
+    }
+}
+}
