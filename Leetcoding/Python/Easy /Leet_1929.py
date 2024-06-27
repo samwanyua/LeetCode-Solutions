02b2b061297d12951bcb03e836687f32eedb267a
samwanyua <samexample8@gmail.com>
2024-06-27 12:33:42 +0300

    Solve concatenation of array  problem

---
+++ Leetcoding/Python/Easy /Leet_1929.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+        return ans
+        
