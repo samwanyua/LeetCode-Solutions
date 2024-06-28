8ec484ad95898dde6b1ad4f8599c41aca50ecd2f
samwanyua <samexample8@gmail.com>
2024-06-28 17:48:12 +0300

    Solve valid parenthesis  problem

---
+++ Leetcoding/src/LeetCodeJava/Java/Easy/Leet_20.java
@@ -0,0 +1,36 @@
+package Easy;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Leet_20 {
+    private static final String OPEN_BRACKETS = "([{";
+    private static final String CLOSING_BRACKETS = ")]}";
+
+
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i =0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(isOpeningBracket(c)){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty() || !isMatching(stack.pop(),c)){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isOpeningBracket(char c){
+        return OPEN_BRACKETS.indexOf(c) != -1;
+    }
+
+    private boolean isMatching(char open, char close){
+        return OPEN_BRACKETS.indexOf(open) == CLOSING_BRACKETS.indexOf(close);
+    }
+}
